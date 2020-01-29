b9e804f77f7773b2d2c251943b65108c02d8b659
mw <mw@electronic-minds.de>
2020-01-29 17:11:55 +0100

    Add a loading bar package

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,3 +1,5 @@
+import {showLoading, hideLoading} from 'react-redux-loading';
+
 import {getInitialData} from '../utils/api';
 import {recieveTweets} from './tweets';
 import {recieveUsers} from './users';
@@ -7,10 +9,13 @@ const AUTHED_ID = 'tylermcginnis';
 
 export function handleInitialData () {
     return (dispatch) => {
+        dispatch(showLoading());
+
         return getInitialData().then(({users, tweets}) => {
             dispatch(recieveTweets(tweets));
             dispatch(recieveUsers(users));
             dispatch(setAuthedUser(AUTHED_ID));
+            dispatch(hideLoading());
         });
     };
 }
